67217873c6030683bf663869894eb4f69703d520
KeonOlszewski <olszkeon@gmail.com>
2020-11-10 20:42:56 -0500

    Update NBAscraper.py

--- NBAscraper.py
+++ NBAscraper.py
@@ -13,8 +13,7 @@ url = "https://www.basketball-reference.com/players/l/lowryky01/gamelog/2020"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
-htmltable = soup.find(id="pgl_basic_playoffs")
-print(htmltable)
+htmltable = soup.find(id="all_pgl_basic")
 table_rows = htmltable.find_all('tr')
 
 table = []
@@ -32,9 +31,17 @@ for row in table:
     if(len(row) >= 26):
          points.append(int(row[26]))
 
+games = []
+for row in table:
+    if(len(row) >= 26):
+         games.append(int(row[0]))
+
 
     
 plt.figure()
-plt.plot(points)
+plt.bar(games, points)
 plt.title('Kyle Lowry Scoring 2019-2020')
+plt.axis([0, 67, 0, 50])
+plt.ylabel('points')
+plt.xlabel('game')
 plt.show()
