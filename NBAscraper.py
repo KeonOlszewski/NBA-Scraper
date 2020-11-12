62d49b90df85e212acd68475d8ac6b170ddfa004
KeonOlszewski <olszkeon@gmail.com>
2020-11-12 11:19:11 -0500

    added ppg in wins vs ppg in losses

--- NBAscraper.py
+++ NBAscraper.py
@@ -4,12 +4,15 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 
-#url = "https://www.basketball-reference.com/players/j/jamesle01/gamelog/2020"
+#url = "https://www.basketball-reference.com/players/j/jamesle01/gamelog/2020" #Lebron James
 
-url = "https://www.basketball-reference.com/players/l/lowryky01/gamelog/2020"
+#url = "https://www.basketball-reference.com/players/l/lowryky01/gamelog/2020" #Kyle Lowry
+
+url = "https://www.basketball-reference.com/players/h/herroty01/gamelog/2020/" #Tyler Herro
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
@@ -26,21 +29,50 @@ for tr in table_rows:
         table.append(row)
 
 
+
+pointsInWin = []
+pointsInLoss = []
 points = []
-for row in table:
-    if(len(row) >= 26):
-         points.append(int(row[26]))
-
 games = []
+numWins = 0
+numLosses = 0 
 for row in table:
     if(len(row) >= 26):
-         games.append(int(row[0]))
+        points.append(int(row[26]))
+        games.append(int(row[0]))
+        if("W" in row[6]):
+            pointsInWin.append(int(row[26]))
+            numWins += 1
+        else:
+            pointsInLoss.append(int(row[26]))
+            numLosses += 1
+
+pointsAvgInWin = np.average(pointsInWin)
+pointsAvgInWin = round(pointsAvgInWin, 2)
+
+pointsAvg = np.average(points)
+pointsAvg = round(pointsAvg, 2)
+
+pointsAvgInLoss = np.average(pointsInLoss)
+pointsAvgInLoss = round(pointsAvgInLoss, 2)
+
+print("=================================================")
 
 
+print("Tyler Herro ppg: ",pointsAvg)
+print("Tyler Herro ppg in wins: ",pointsAvgInWin," in ", numWins," wins")
+print("Tyler Herro ppg in losses: ",pointsAvgInLoss," in ", numLosses, "losses")
+
+print("=================================================")         
+
+
+
+
+  
     
 plt.figure()
 plt.bar(games, points)
-plt.title('Kyle Lowry Scoring 2019-2020')
+plt.title('Tyler Herro Scoring 2019-2020')
 plt.axis([0, 67, 0, 50])
 plt.ylabel('points')
 plt.xlabel('game')
